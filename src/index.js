635cc9aebad6e04d82ab0b5cc2b13b0978110d6c
Zach <zbayard@gmail.com>
2021-01-13 14:45:13 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+const form = document.querySelector("#create-task-form")
+const description = document.querySelector("input#new-task-description")
+const tasks = document.querySelector("#tasks")
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  form.addEventListener('submit', function(event){
+    event.preventDefault()
+  
+    const taskInput = description.value
+  
+    const newLi = document.createElement('li')
+  
+    newLi.textContent = taskInput
+  
+    tasks.appendChild(newLi)
+      
+    
+  
+    form.reset()
+  })
+  
+
 });
+
+
+
+
